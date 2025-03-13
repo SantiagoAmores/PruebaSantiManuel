c87229a148efc71a62b417888296f62c42347bb4
SantiagoAmores <santi251998@gmail.com>
2025-03-13 08:34:54 +0100

    Script

---
+++ PruebaSantiManuel.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PruebaSantiManuel : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+   void awake()
+   {
+    int variable = 2
+   }
+
+   void start()
+   {
+
+   }
+
+   void update()
+   {
+
+   }
+}
+
+
+
+
+
+
